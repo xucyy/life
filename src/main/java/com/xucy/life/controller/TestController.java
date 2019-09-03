7fd728fe0ae33805a074aa74a28d576f73acadaa
xuchenyang <xuchenyang@zhihu.com>
2019-09-03 17:16:29 +0800

    连接数据库

---
+++ src/main/java/com/xucy/life/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.xucy.life.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author xucy
+ * @Date 2019-09-03 17:07
+ * @Description
+ **/
+
+@RestController
+@RequestMapping("/testCon")
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test(){
+        return "成功";
+    }
+}
